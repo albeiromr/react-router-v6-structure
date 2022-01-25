2758af24cfa492847cb0d2e1ca537bdf659261d2
Albeiro Marìn <xlandru@hotmail.com>
2022-01-24 19:32:16 -0500

    Termina la implementación de rutas anidadas

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,17 +1,29 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Error404Component, HomeComponent, LoginComponent, PrivateRouteComponent, PublicRouteComponent } from './testing-components/test-components.component';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+import { 
+    Error404Component, 
+    HomeComponent, 
+    LoginComponent, 
+    NestedIndexRoute, 
+    NestedRouteOne, 
+    NestedRouteTwo, 
+    PrivateRouteComponent, 
+    PublicRouteComponent 
+} from './testing-components/test-components.component';
 import  App  from '../App'
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="" element={<App />}>
+                <Route element={<App />}>
                     <Route path="/" element={<HomeComponent />}/>
                     <Route path="/login" element={<LoginComponent />} />
                     <Route path="/public-route" element={<PublicRouteComponent />}/>
-                    <Route path="/private-route" element={<PrivateRouteComponent />}></Route>
-                    <Route path="/404" element={<Error404Component />} />
+                    <Route path="/private-route" element={<PrivateRouteComponent />}>
+                        <Route index element={<NestedIndexRoute />}/>
+                        <Route path="nested-route-one" element={<NestedRouteOne />}/>
+                        <Route path="nested-route-two" element={<NestedRouteTwo />}/>
+                    </Route>
                     <Route path="*" element={<Error404Component />} />
                 </Route>
             </Routes>
--- src/router/testing-components/test-components.component.tsx
+++ src/router/testing-components/test-components.component.tsx
@@ -1,13 +1,14 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import "./test-styles.scss";
 
 export const PublicRouteComponent = () => {
     return (
         <div className="public-route">
             <h1>Soy una ruta publica</h1>
-
-            <NavLink to="/public-route" className={(navData) => navData.isActive ? "selected-url" : "link" }>Public route</NavLink>
-            <NavLink to="/" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go to home</NavLink>
+            <nav className="navigation-bar">
+                <NavLink to="/public-route" className={(navData) => navData.isActive ? "selected-url" : "link" }>Public route</NavLink>
+                <NavLink to="/" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go to home</NavLink>
+            </nav>
         </div>
     )
 }
@@ -17,8 +18,10 @@ export const LoginComponent = () => {
     return (
         <div className="login">
             <h1>Soy la pantalla de login</h1>
-            <NavLink to="/login" className={(navData) => navData.isActive ? "selected-url" : "link" }>Login</NavLink>
-            <NavLink to="/" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go to home</NavLink>
+            <nav className="navigation-bar">
+                <NavLink to="/login" className={(navData) => navData.isActive ? "selected-url" : "link" }>Login</NavLink>
+                <NavLink to="/" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go to home</NavLink>
+            </nav>
             
         </div>
     )
@@ -28,11 +31,13 @@ export const HomeComponent = () => {
     return (
         <div className="home">
            <h1>Soy el Home</h1>
-           <NavLink to="/" className={(navData) => navData.isActive ? "selected-url" : "link" }>Home</NavLink>
-           <NavLink to="/login" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go to login</NavLink>
-           <NavLink to="/public-route" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go to public route</NavLink>
-           <NavLink to="/private-route" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go to private route</NavLink>
-           <NavLink to="/404" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go 404 page</NavLink>
+           <nav className="navigation-bar">
+               <NavLink to="/" className={(navData) => navData.isActive ? "selected-url" : "link" }>Home</NavLink>
+               <NavLink to="/login" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go to login</NavLink>
+               <NavLink to="/public-route" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go to public route</NavLink>
+               <NavLink to="/private-route" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go to private route</NavLink>
+               <NavLink to="/404" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go 404 page</NavLink>
+           </nav>
         </div>
     )
 }
@@ -41,8 +46,10 @@ export const Error404Component = () => {
     return (
         <div className="error-404">
             <h1>pagina no encontrada</h1>
-            <NavLink to="/404" className={(navData) => navData.isActive ? "selected-url" : "link" }>Error 404</NavLink>
-            <NavLink to="/" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go to home</NavLink>
+            <nav className="navigation-bar">
+                <NavLink to="/404" className={(navData) => navData.isActive ? "selected-url" : "link" }>Error 404</NavLink>
+                <NavLink to="/" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go to home</NavLink>
+            </nav>
         </div>
     )
 }
@@ -52,10 +59,13 @@ export const PrivateRouteComponent = () => {
     return (
         <div className="private-route">
             <h1>Soy una ruta Privada</h1>
-            <nav>
+            <nav className="navigation-bar">
                 <NavLink to="/" className={(navData) => navData.isActive ? "selected-url" : "link" }>Go to home</NavLink>
-                <NavLink to="/private-route" className={(navData) => navData.isActive ? "selected-url" : "link" }>Private route</NavLink>
+                <NavLink to="/private-route" className={(navData) => navData.isActive ? "selected-url" : "link" } end >Private route</NavLink>
+                <NavLink to="/private-route/nested-route-one" className={(navData) => navData.isActive ? "selected-url" : "link" }>Nested route #1</NavLink>
+                <NavLink to="/private-route/nested-route-two" className={(navData) => navData.isActive ? "selected-url" : "link" }>Nested route #2</NavLink>
             </nav>
+            <Outlet />
         </div>
     )
 }
